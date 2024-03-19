939459089b67c71381217c55b70b32963eec9cfa
AndreyZ <andreyzv2525@gmail.com>
2024-03-19 17:22:27 +0300

    registration url

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,9 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, flash, redirect, url_for, request
 from app import app
+from app import db
 from app.forms import LoginForm
+from app.forms import RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
@@ -28,6 +30,24 @@ def index():
         }
     ]
     return render_template("index.html", title='Home Page', posts=posts)
+
+
+# Регистрация
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+# Разрегистрация?
+
 
 # Логин
 @app.route('/login', methods=['GET', 'POST'])
