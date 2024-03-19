1d6ca7f3010f1e34307179f9dc0436e58a374903
AndreyZ <andreyzv2525@gmail.com>
2024-03-19 12:35:13 +0300

    add context for flask shell

--- blog.py
+++ blog.py
@@ -1,3 +1,8 @@
 # Файл для запуска
+from app import app, db
+from app.models import User, Post
 
-from app import app
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
