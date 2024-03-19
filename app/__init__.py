c89ae00d01f8b38e501e9911c84984fc93c6f134
AndreyZ <andreyzv2525@gmail.com>
2024-03-19 10:36:33 +0300

    add modules for db

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
+# Файл для насторойки приложения
+
 # -*- coding: utf-8 -*-
+import sqlite3
 from flask import Flask
 from config import Config
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-from app import routes
+from app import routes, models
