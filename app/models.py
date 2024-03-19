bfe728365f01e6c7037f012ab1f9ff0cb6050f98
AndreyZ <andreyzv2525@gmail.com>
2024-03-19 11:20:05 +0300

    Post data model and the relationship with the user

--- app/models.py
+++ app/models.py
@@ -1,13 +1,29 @@
 # Модели представления данных в бд
 
-
+from datetime import datetime
 from app import db
 
+
+# Пользователь
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # Показываем связь один ко многим (указывается от одного)
+    # Высокоуровневое представление отношения user и post
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+# Запись в блог
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
