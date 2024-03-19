3b3437e4fec6feb4a2b86c22dfbb6e2fc330bb4c
AndreyZ <andreyzv2525@gmail.com>
2024-03-19 18:59:44 +0300

    new fields in user model

--- app/models.py
+++ app/models.py
@@ -14,6 +14,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     # Показываем связь один ко многим (указывается от одного)
     # Высокоуровневое представление отношения user и post
     posts = db.relationship('Post', backref='author', lazy='dynamic')
