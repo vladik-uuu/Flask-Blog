3733bf8a07ff209e1f89e3ead42680bac5500e3f
AndreyZ <andreyzv2525@gmail.com>
2024-03-20 16:33:42 +0300

    route for form about me

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ from app.forms import RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from datetime import datetime
-
+from app.forms import EditProfileForm
 
 # главная страничка
 @app.route('/')
@@ -92,3 +92,20 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
